1a02a8cf153d3db124fdfdd64d4687d76e97f2ef
Apex-droid <59209023+Apex-droid@users.noreply.github.com>
2023-02-21 18:58:11 +0300

    Add files via upload

---
+++ calc/src/main/java/com/example/demo/Model.java
@@ -0,0 +1,153 @@
+package com.example.demo;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
+import java.util.Formatter;
+import java.util.Objects;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+public class Model extends expression {
+
+    //private static expression expr;
+    private String text = "";
+    
+    private History history = new History();
+    private int histIndex = history.size() - 1;
+     Model() {
+    	 
+    	  
+         try (FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\ALEX\\Desktop\\save.ser");
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream))
+         {objectOutputStream.writeObject(history);}
+         catch (IOException e) {
+             System.err.println("Error writing from file");
+         }
+         
+         //catch(IOException e) {
+         //throw new RuntimeException(e);
+
+     }
+
+    interface Listener {
+         void txt_changer(String txt);
+    }
+
+
+    Listener listener;
+
+    public void rebuild_ex() {
+        rebuild_ex(text);
+    }
+    public void edit(String new_txt) {                                                            
+    	if (text.equals("input error "))
+    		text = "";
+        if (new_txt.equals("<-")){
+        	if (!text.equals("") && text.charAt(text.length()- 1) != ' ')
+        		text = StringUtils.chop(text);
+        }
+        else {
+        	if (new_txt.matches(siNco))
+        		text += new_txt + "(";
+        	else 
+        		text += new_txt;
+        	
+        }
+        if (this.listener != null)
+            listener.txt_changer(text);
+    }
+    public void enter()  {
+        NumberFormat nf = new DecimalFormat ("0.######");
+        if (inputChecker.Check(text))
+        {
+        	if(half_done) {
+        		parse_half(text);
+        		history.add_op(text);
+        		text = nf.format(calc());
+        		history.add_res(text);
+        		histIndex =history.size() - 1;
+        		half_done = false;
+        		save_hist();
+        	}
+        	else {
+        		rebuild_ex(text);
+                if(!half_done) {
+                	history.add_op(text);
+                	reverse_stack();
+                	text = nf.format(calc());
+                	history.add_res(text);
+                	histIndex = history.size() - 1;
+                	save_hist();
+                }
+                else
+                    text = "enter x: ";
+        	}
+        }
+        else
+        	text = "input error ";
+        listener.txt_changer(text);
+    }
+    public double y_graf_func(double x) {
+        rebuild_ex(text);
+        get_X(x);
+        return (calc());
+    }
+    public boolean isHistEmpty() {
+        if (history.size() == 0)
+            return true;
+        else
+            return false;
+    }
+    //@Override
+    protected void save_hist()  {
+        //FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Username\\Desktop\\save.ser");
+        //ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        //History savedGame = (History) objectInputStream.readObject();
+        try (FileInputStream fileInputStream = new FileInputStream("\\Users\\ahelper\\Desktop\\save.ser");
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream))
+        {History savedGame = (History) objectInputStream.readObject();}
+        catch (IOException | ClassNotFoundException e) {
+            System.err.println("save error");
+        }
+    }
+
+    public String[] history_array() {
+    	
+        return history.result_array();
+    }
+    
+    public void takeHistoryUp() {
+    	if (history.size()!= 0){
+    		text = history.get_operation(histIndex);
+    		histIndex--;
+    		if (histIndex < 0)
+    		histIndex = history.size() - 1;
+    		if (this.listener != null)
+            listener.txt_changer(text);
+    	}
+    }
+ public void takeHistoryDown() {
+	 
+    if (history.size()!= 0){	
+    	text = history.get_operation(histIndex);
+    	histIndex++;
+    	if (histIndex == history.size())
+    		histIndex = 0;
+    	if (this.listener != null)
+            listener.txt_changer(text);
+    }
+    }
+public void expFromHist(int histIndex) {
+	
+	text = history.get_operation(histIndex);
+	if (this.listener != null)
+        listener.txt_changer(text);
+}
+public void HistoryClear() {
+	history.clear();
+	histIndex = 0;
+}
+	// TODO Auto-generated method stub
+	
+
+
+}
---
+++ calc/src/main/java/com/example/demo/inputChecker.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class inputChecker {
+	
+	private static Stack<Character> scope_stack = new Stack<>(); 
+	private static String scopeO ="(\\()";
+	private static String scopeC = "(\\))";
+	private static String hp_sign =  "((\\*)|(\\/)|(//%))";
+	private static String lp_sign = "((\\+)|(\\-))";
+	private static String sin_co = " ((cos)|(sin)|(asin)|(acos)|(tan)|(atan)|(ln)|(log)|(sqrt))";
+	private static String lpSingE = lp_sign + "(" + scopeC + "|" +  hp_sign + "|" +  "(\\^)" + ")";
+	private static String hpSingE = hp_sign + "(" + scopeC  + "|" +  "(\\^)" + "|" + hp_sign + ")";
+	private static String DDE = "(\\d+\\.(\\d+)?)(\\d+\\.(\\d+)?)";
+	private static String DE = "(\\d+\\.?(\\d+)?)((X)|" + sin_co + "|" + scopeO + ")";
+	private static String XE = "(X)((\\d+\\.?(\\d+)?)|" + sin_co + "|" + scopeO + "|" + "(X)" + ")";
+	private static String scopeOE = scopeO + "(" + hp_sign + "|" + scopeC + "^" + ")";
+	private static String scopeCE = scopeC + "(" + "(X)" +  "|" + "(\\d+\\.?(\\d+)?)" + "|" + scopeO +
+	"|" + sin_co + ")";
+	private static String powE = "(\\^)" +  "(" + hp_sign +  "|" + lp_sign  + "|(\\^)" + "|" + scopeC + ")";
+	private static String three_lpE = lp_sign + lp_sign + lp_sign  ;
+	private static String ExpRegex = lpSingE +  "|" + hpSingE +  "|" + DDE +  
+	"|" + DE +  "|" + XE +  "|" + scopeOE 
+	+ "|" + scopeCE + "|" + powE + "|" + three_lpE ;
+	private static Pattern p = Pattern.compile(ExpRegex);
+	private static Pattern liters = Pattern.compile("[a-z]");
+	
+	public static boolean scopeCheck(String exp)
+	{
+		for(int i = 0; i < exp.length(); i++)
+			if(exp.charAt(i) == ')')
+				scope_stack.push(exp.charAt(i));
+			else if(exp.charAt(i) == ')'){
+				if (scope_stack.isEmpty())
+					return false;
+			else if (scope_stack.peek() == '(') 
+					scope_stack.pop();				 
+			}
+		if (scope_stack.isEmpty())
+			return true;
+		return false;
+	}
+	
+	public static boolean Check(String exp)
+	{
+		Matcher m = p.matcher(exp);
+		if (scopeCheck(exp)){
+			if (m.find())
+				return false;
+			m = liters.matcher(exp);
+			while(m.find())
+				if(!exp.substring(m.start(),m.end()).matches(sin_co))
+					return false;
+			return true;
+		}
+		else 
+			return false;
+	}
+}
